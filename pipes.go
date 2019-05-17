3db5b993f1aa75aa48fa3e0964f5f37073007b09
Tomas Grubliauskas <v3nom@MacPro.local>
2019-05-17 18:00:09 +0200

    initial commit

---
+++ pipes.go
@@ -0,0 +1,57 @@
+package pipes
+
+import (
+	"context"
+	"net/http"
+)
+
+// Middleware function.
+type Middleware = func(ctx context.Context, w http.ResponseWriter, r *http.Request, next func())
+
+// ContextConstructor context constructor function.
+type ContextConstructor = func(w http.ResponseWriter, r *http.Request) context.Context
+
+// NewPipeline creates new empty pipeline.
+func NewPipeline(contextConstructor ContextConstructor) Pipeline {
+	p := Pipeline{
+		middlewares:        []Middleware{},
+		contextConstructor: contextConstructor,
+	}
+	return p
+}
+
+// Pipeline instance.
+type Pipeline struct {
+	middlewares        []Middleware
+	contextConstructor ContextConstructor
+}
+
+// Run returns a function which can run the pipeline.
+func (p Pipeline) Run() func(w http.ResponseWriter, r *http.Request) {
+	var next func()
+	return func(w http.ResponseWriter, r *http.Request) {
+		middlewareCount := len(p.middlewares)
+		if middlewareCount == 0 {
+			return
+		}
+
+		ctx := p.contextConstructor(w, r)
+		i := -1
+
+		next = func() {
+			i++
+			if i <= middlewareCount-1 {
+				p.middlewares[i](ctx, w, r, next)
+			}
+		}
+		next()
+	}
+}
+
+// Use adds a new middleware to the pipeline and returns a new pipeline.
+func (p Pipeline) Use(m Middleware) Pipeline {
+	return Pipeline{
+		middlewares:        append(p.middlewares, m),
+		contextConstructor: p.contextConstructor,
+	}
+}
---
+++ pipes_test.go
@@ -0,0 +1,80 @@
+package pipes
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreatePipeline(t *testing.T) {
+	pipeline := createPipeline()
+	pipeline1 := pipeline.Use(pipelineA)
+	pipeline2 := pipeline1.Use(pipelineB)
+
+	if len(pipeline.middlewares) != 0 {
+		t.Fatal("Expected 0 middleware")
+	}
+	if len(pipeline1.middlewares) != 1 {
+		t.Fatal("Expected 1 middleware")
+	}
+	if len(pipeline2.middlewares) != 2 {
+		t.Fatal("Expected 2 middleware")
+	}
+}
+
+func TestEmptyPipeline(t *testing.T) {
+	pipeline := createPipeline()
+
+	// Request
+	req, err := http.NewRequest("GET", "/test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Request handling
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(pipeline.Run())
+	handler.ServeHTTP(recorder, req)
+}
+
+func TestPipelineNext(t *testing.T) {
+	pipeline := createPipeline().Use(pipelineA).Use(pipelineB)
+
+	// Request
+	req, err := http.NewRequest("GET", "/test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Request handling
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(pipeline.Run())
+	handler.ServeHTTP(recorder, req)
+
+	pipelineHeader := recorder.HeaderMap.Get("pipeline")
+	if pipelineHeader != "B" {
+		t.Fatalf("Pipeline header. Expected: %v, Actual: %v", "B", pipelineHeader)
+	}
+}
+
+func createPipeline() Pipeline {
+	return NewPipeline(func(w http.ResponseWriter, r *http.Request) context.Context {
+		return context.TODO()
+	})
+}
+
+func pipelineA(ctx context.Context, w http.ResponseWriter, r *http.Request, next func()) {
+	log.Println("PipelineA")
+	w.Header().Set("pipeline", "A")
+
+	next()
+}
+
+func pipelineB(ctx context.Context, w http.ResponseWriter, r *http.Request, next func()) {
+	log.Println("PipelineB")
+	w.Header().Set("pipeline", "B")
+
+	next()
+}
